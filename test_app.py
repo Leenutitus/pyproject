e44cf7f0a0e14470786973ebca3679f99d2d3075
LEENU TITUS <132454203+Leenutitus@users.noreply.github.com>
2023-05-22 10:47:41 +0530

    Update and rename main.py to test_app.py

---
+++ test_app.py
@@ -0,0 +1,35 @@
+
+
+# test_app.py
+
+import unittest
+
+from app import app
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+
+        eval.testing = True
+
+        self.app = app.test_client()
+
+
+    def test_hello_world(self):
+
+        response = self.app.get('/')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data, b'Hello, World!')
+
+
+if __name__ == '__main__':
+
+    unittest.main()
+
+
+
+
+
